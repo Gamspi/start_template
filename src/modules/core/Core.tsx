6aaef6d8b849fa6253e6bda38f5b2ca2ea7818a0
Gamspi <niko.tatt007@gmail.com>
2022-07-02 22:28:36 +0700

    стартовый шаблон react v.1

---
+++ src/modules/core/Core.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.scss'
+const Core = () => {
+    return (
+        <div className='Core'>
+
+        </div>
+    );
+};
+
+export default React.memo(Core);
