5ca658a2becfc5aa98508904125fb0e72323c2b6
Gamspi <niko.tatt007@gmail.com>
2022-08-29 14:37:26 +0700

    перенастроен webpack

--- webpack/config.js
+++ webpack/config.js
@@ -2,11 +2,13 @@ const { merge } = require('webpack-merge')
 const { configBase } = require('./config.base')
 const { configDev } = require('./config.dev')
 const { configProd } = require('./config.prod')
-
-module.exports = (env) => {
-  if (env.development) {
+const mode = process.env.NODE_ENV
+module.exports = () => {
+  if (mode === 'development') {
+    console.log('Mode env: '+mode)
     return merge(configBase, configDev)
-  } else if (env.production) {
+  } else if (mode === 'production') {
+    console.log('Mode env: '+mode)
     return merge(configBase, configProd)
   }
   throw new Error('no matching configuration was found :(((((')
